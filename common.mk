9f69fbf1440733046e6eed7665760f27cae3415e
Jens Lody <jens@jenslody.de>
2023-01-04 22:33:39 +0100

    Revert "sm7125: correct SystemUI apk name and path"
    This reverts commit 7b32aa26406eeead1a55f8c48dbd5a96817adf85.

--- common.mk
+++ common.mk
@@ -461,7 +461,7 @@ WITH_DEXPREOPT_DEBUG_INFO := false
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
-    SystemUIGoogle
+    SystemUI
     
 # Prop files
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
