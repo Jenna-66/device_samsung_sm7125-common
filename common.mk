a043c26645e5f93602c5ddbc0922bfbb4cf203e2
Jenna-they-them <jenna@jenslody.de>
2024-03-13 13:42:48 +0100

    sm7125-common: build lineage livedisplay
    Signed-off-by: Jenna-they-them <jenna@jenslody.de>

--- common.mk
+++ common.mk
@@ -224,6 +224,10 @@ PRODUCT_PACKAGES += \
     libcrypto-v33 \
     libkeymaster4_1support.vendor
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.samsung-qcom.sm7125
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
