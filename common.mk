Simon1511/android_device_samsung_sm7125-common

sm7125-common: Switch to AIDL camera HAL
Change-Id: Idc9ccc33c4ba9c2c6668d145405da623cf4dfc49

common.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service.samsung \
+    android.hardware.camera.provider-service.samsung \
     libgrallocusage.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
 

Jenna-66/device_samsung_sm7125-common

common.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service.samsung \
+    android.hardware.camera.provider-service_32.samsung \
     libgrallocusage.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 