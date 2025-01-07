a6644845ca9a11f437cabd64c41f7a643b78001a
Aryan <basam.aryan@gmail.com>
2025-01-07 14:24:12 +0530

    sm7125-common: display: Convert to blueprint
    Change-Id: Ia7f1b0dc373feb2cd2a7db433c162626e5bce29d
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- common.mk
+++ common.mk
@@ -139,7 +139,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service \
     libtinyxml \
     libtinyxml2 \
-    hwcomposer.qcom \
+    hwcomposer.sm7125 \
     libqdMetaData \
     libdisplayconfig.qti \
     vendor.qti.hardware.display.mapper@1.1.vendor \
